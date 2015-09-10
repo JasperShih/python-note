0eb6581f268eedbb5e353612dc5b406c8d1dbb6c
RapidGrowth <jokergreem@gmail.com>
2015-09-10 22:56:24 +0800

    b

--- with.py
+++ with.py
@@ -1,17 +1,20 @@
 class Sample:
+    def __init__(self):
+        pass
+
     def __enter__(self):
-        return 5/2 == 2
+        return 5 / 2 == 2
 
     def __exit__(self, type, value, trace):
         print "type:", type
         print "value:", value
         print "trace:", trace
-        #return True
+        # return True
+        # or return isinstance(value, TypeError)
 
 
 def do_something():
     bar = 1 / 0
-
 
 
 with Sample() as sample:
@@ -21,3 +24,30 @@ with Sample() as sample:
 print "yy"
 print "yy"
 print "yy"
+
+""" deal in exception in __exit__
+or like that:
+
+try:
+    with open( "a.txt" ) as f :
+        do something
+except xxxError:
+    do something about exception
+
+or
+
+try:
+    with open( "a.txt" ) as f :
+        do something
+        .
+        .
+    .
+    .
+    .
+    .
+    .
+except xxxError:
+    do something about exception
+
+    """
+
