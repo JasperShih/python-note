7f037cf2e790417eab774ba78d16c972203d9d7e
RapidGrowth <jokergreem@gmail.com>
2015-10-13 01:55:59 +0800

    i2

--- try_except.py
+++ try_except.py
@@ -13,3 +13,23 @@ except ZeroDivisionError:
 
 except:
     print "got everythings!"
+
+
+# =====================================
+
+
+def login(self):
+        try:
+            gmail_login_URL = """https://accounts.google.com/ServiceLogin?service=mail&passive=true&rm=false&continue=https://mail.google.com/mail/&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&lp=1&hl=zh-CN#identifier"""
+
+            self.driver.get(gmail_login_URL)
+            self.driver.find_element_by_id("Email").send_keys(self.email)
+            self.driver.find_element_by_id("next").send_keys(Keys.ENTER)
+            self.driver.find_element_by_id("Passwd").send_keys(self.password)
+            self.driver.find_element_by_id("signIn").send_keys(Keys.ENTER)
+
+            return True
+        except Exception as error:
+            print error
+
+        return False
