dfefb71a183a0b4d7f1dbd573152d604c96e7dea
RapidGrowth <jokergreem@gmail.com>
2016-01-02 09:46:44 +0800

    random

--- random.py
+++ random.py
@@ -1,6 +1,6 @@
+# -*- coding: utf8 -*-
 import random
 
-__author__ = 'Jasper'
 
 agents = ["Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
           "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
@@ -20,3 +20,50 @@ agents = ["Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) C
 
 # pick one element of agents
 random.choice(agents)
+
+
+隨機整數：
+>>> import random
+>>> random.randint(0,99)
+21
+
+隨機選取0到100間的偶數：
+>>> import random
+>>> random.randrange(0, 101, 2)
+42
+
+隨機浮點數：
+>>> import random
+>>> random.random()
+0.85415370477785668
+>>> random.uniform(1, 10)
+5.4221167969800881
+
+隨機字元：
+>>> import random
+>>> random.choice('abcdefg&#%^*f')
+'d'
+
+多個字元中選取特定數量的字元：
+>>> import random
+random.sample('abcdefghij',3)
+['a', 'd', 'b']
+
+多個字元中選取特定數量的字元組成新字串：
+>>> import random
+>>> import string
+>>> string.join(random.sample(['a','b','c','d','e','f','g','h','i','j'], 3)).r
+eplace(" ","")
+'fih'
+
+隨機選取字串：
+>>> import random
+>>> random.choice ( ['apple', 'pear', 'peach', 'orange', 'lemon'] )
+'lemon'
+
+洗牌：
+>>> import random
+>>> items = [1, 2, 3, 4, 5, 6]
+>>> random.shuffle(items)
+>>> items
+[3, 2, 5, 6, 4, 1]
